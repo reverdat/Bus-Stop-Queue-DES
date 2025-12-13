93761006d6365d894ebe5f9879bc68a563302102
Pau Soler Valadés <pau.soler.valades@gmail.com>
2025-12-13 17:16:13 +0100

    Simulant el fucking autobus xd

--- assignment2_preentrega/event-scheduling/src/main.zig
+++ assignment2_preentrega/event-scheduling/src/main.zig
@@ -15,10 +15,11 @@ pub fn main() !void {
 
     const K: u64 = 9;
     const X: u64 = 3;
-    const mu: f64 = 2.0;
-    const lambda: f64 = 3.0;
+    const mu: f64 = 1.0;
+    const lambda: f64 = 0.5;
 
-    try eventSchedulingMM1(gpa, lambda, mu, X, K, 1000000);
+    print("Starting and MM1 silulation\n", .{});
+    try eventSchedulingMM1(gpa, lambda, mu, X, K, 10000);
 }
 
 const ServerState = enum { free, busy };
@@ -54,6 +55,9 @@ fn eventSchedulingMM1(gpa: Allocator, lambda: f64, mu: f64, x: u64, k: u64, hori
     var num_clients_in_system: u64 = 0;
     var server_state: ServerState = ServerState.free;
     var t_clock: f64 = 0.0;
+    
+    var area_under_q: f64 = 0.0;
+    var last_event_time: f64 = 0.0;
 
     var prng = Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
@@ -66,12 +70,15 @@ fn eventSchedulingMM1(gpa: Allocator, lambda: f64, mu: f64, x: u64, k: u64, hori
     const arrival_event: Event = Event{ .time = first_arrival, .type = EventType.arrival, .client_id = client_counter };
 
     try hp.push(gpa, arrival_event);
-
+    
     while (t_clock <= horizon and hp.len() > 0) : (processed_events += 1) {
         const next_event = hp.pop().?;
         t_clock = next_event.time;
+        
+        // estem fent la integral :3
+        area_under_q += @as(f64, @floatFromInt(num_clients_in_system)) * (t_clock - last_event_time);
+        last_event_time = t_clock;
 
-        // TODO: METRIQUES DE CUES
         switch (next_event.type) {
             EventType.arrival => {
                 client_counter += 1;
@@ -80,6 +87,7 @@ fn eventSchedulingMM1(gpa: Allocator, lambda: f64, mu: f64, x: u64, k: u64, hori
                 const next_arrival_time = try rexp(f64, lambda, &gen);
                 const new_arrival_event: Event = Event{ .time = t_clock + next_arrival_time, .type = EventType.arrival, .client_id = client_counter };
                 try hp.push(gpa, new_arrival_event);
+        
                 if (server_state == ServerState.free) {
                     server_state = ServerState.busy;
                     const next_service_time = try rexp(f64, mu, &gen);
@@ -98,70 +106,14 @@ fn eventSchedulingMM1(gpa: Allocator, lambda: f64, mu: f64, x: u64, k: u64, hori
                     server_state = ServerState.free;
                 }
             },
+            else => {}, 
         }
-        print("{any}\n", .{hp.list.items});
     }
+
+    const average_clients = area_under_q / t_clock;
+
+    print("----- Simulation finished! -----\n", .{});
+    print("\tDuration: \t\t{e} \n\tEvents processed: \t{d} \n\tAverage clients (L): \t{e}", .{t_clock, processed_events, average_clients});
 }
 
-fn eventSchedulingBus(gpa: Allocator, lambda: f64, mu: f64, x: u64, horizon: f64) !void {
-    _ = x;
 
-    var hp = heap.Heap(Event).init();
-    defer hp.deinit(gpa);
-    var client_counter: u64 = 1;
-    var processed_events: u64 = 0;
-    var num_clients_in_system: u64 = 0;
-    var server_state: ServerState = ServerState.free;
-    var t_clock: f64 = 0.0;
-    var current_bus_capacity: u64 = 0;
-
-    const boarding_time: f64 = 1e-8;
-
-    var prng = Random.DefaultPrng.init(blk: {
-        var seed: u64 = undefined;
-        try std.posix.getrandom(std.mem.asBytes(&seed));
-        break :blk seed;
-    });
-    var gen = prng.random();
-
-    const first_passenger_time: f64 = try rexp(f64, lambda, &gen);
-    const passenger_event: Event = Event{ .time = first_passenger_time, .type = EventType.arrival, .client_id = 1 };
-    try hp.push(gpa, passenger_event);
-
-    client_counter += 1;
-    const first_bus_time: f64 = try rexp(f64, mu, &gen);
-    const bus_event: Event = Event{ .time = first_bus_time, .type = EventType.service, .client_id = 2 };
-    try hp.push(gpa, bus_event);
-
-    client_counter = 3; // Comencem a contar a partir del tercer esdeveniment.
-
-    while (t_clock <= horizon and hp.len() > 0) : (processed_events += 1) {
-        const next_event = hp.pop().?;
-        t_clock = next_event.time;
-
-        // TODO: METRIQUES DE CUES
-        switch (next_event.type) {
-            EventType.arrival => {
-                num_clients_in_system += 1;
-
-                client_counter += 1;
-                const next_arrival_time = try rexp(f64, lambda, &gen);
-                const new_arrival_event: Event = Event{ .time = t_clock + next_arrival_time, .type = EventType.arrival, .client_id = client_counter };
-                try hp.push(gpa, new_arrival_event);
-            },
-            EventType.service => {
-                //num_clients_in_system -= 1;
-                current_bus_capacity = k;
-
-                if (num_clients_in_system > 0) {
-                    const next_service_time: f64 = try rexp(f64, mu, &gen);
-                    const next_service_event: Event = Event{ .time = t_clock + next_service_time, .type = EventType.service, .client_id = next_event.client_id + 1 };
-                    try hp.push(gpa, next_service_event);
-                } else {
-                    server_state = ServerState.free;
-                }
-            },
-        }
-        print("{any}\n", .{hp.list.items});
-    }
-}
